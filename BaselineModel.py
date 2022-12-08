fbca1811ed159a694c7e15e57897f559a9a6f489
simontodreas <59842298+simontodreas@users.noreply.github.com>
2022-12-08 20:37:50 +0000

    added baseline

--- BaselineModel.py
+++ BaselineModel.py
@@ -4,15 +4,14 @@ from sklearn.linear_model import LinearRegression
 
 train_data = pd.read_csv('pitches.csv')
 test_data = pd.read_csv('hidden.csv')
-print(len(train_data))
 model = LinearRegression()
 
-X_train = train_data.iloc[:,0:11]
-Y_train = train_data.iloc[:,11]
-X_test = test_data.iloc[:,0:11]
 Y_test = test_data.iloc[:,11]
+baseline_pred = np.zeros(Y_test.shape)
 
-model.fit(X_train, Y_train)
-r_sq = model.score(X_train, Y_train)
-print(r_sq)
-#print(train_data.dtypes)
+def score(pred):
+    dif = (Y_test - pred)**2
+    return(np.mean(dif))
+
+test0 = score(baseline_pred)
+print(test0)
